ef17e04ab66468a790fabe07b53978360c3d6cb6
Saurav-KumarSingh <sauravkumarsingh093@gmail.com>
2024-07-04 21:56:14 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AddTodo from './comonent/AddTodo'
+import Todo from './comonent/Todo'
+
+const App = () => {
+  return (
+    <div>
+      <AddTodo/>
+      <Todo/>
+    </div>
+  )
+}
+
+export default App
